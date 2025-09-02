0dd7a133c466ce0944364ecf04cb97694472559c
Dayanand Patil <pdayanand03@gmail.com>
2025-09-02 14:19:00 +0000

    Implement function called countUniqueValue whcih accepts a sorted array,  and counts the unique value in array. There can be negative numbers in array but it will always be sorted countUniqueValue([1,1,1,1,1,2]) //2 countUniqueValue([1,2,3,4,4,4,7,7,12,12,13]) //7 countUniqueValue([]) //0 countUniqueValue([-2,-1,-1,0,1]) //4

---
+++ Code5.maxSubArraySum.SlidingWindow.js
@@ -0,0 +1,53 @@
+/*
+Write a function called maxSumArraySum which accepts an array of integer and a number called n.
+The function should caluclate the maximum sum of n consecutive elements in the array
+maxSumArraySum([1,2,5,2,8,1,5],2) //10
+maxSumArraySum([1,2,5,2,8,1,5],4) //17
+maxSumArraySum([4,2,1,6],2) //7
+maxSumArraySum([4,2,1,6,2],4) //13
+maxSumArraySum([],4) //null
+*/
+
+// //Mine Approach
+// function maxSumArraySum(arr, num){
+//     if(!arr.length){
+//         return null;
+//     }
+//     let count=0,prevCount;
+//     for(let i=0;i<arr.length;i++){
+//         for(let j=i; j<i+num; j++){
+//             count=count+arr[j];
+//         }
+//         if(prevCount<count || !i){
+//             prevCount = count;
+//         }
+//         count=0;
+//     }
+//     return prevCount;
+// }
+
+//By using SlidingWindow Method
+function maxSumArraySum(arr, num) {
+    if(arr.length < num){
+        return null;
+    }
+    let maxSum =0,tempSum, i;
+    for(i=0; i<num; i++){
+        maxSum=maxSum+arr[i];
+    } //Window Frame sum created
+    tempSum = maxSum;
+    for (j=i;j<arr.length;j++){
+        tempSum = tempSum + arr[j] -arr[j-num];
+        if(tempSum > maxSum){
+            maxSum = tempSum;
+        }
+    }
+    return maxSum
+}
+
+/*
+Note:
+for mine sceneario if the n and array length are closer in worst case it will n*n-1 which is o(n^2)
+For Sliding window it will be o(n)
+So the sliding window will be the efficent method to solve this problem
+*/
